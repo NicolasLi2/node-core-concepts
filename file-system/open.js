63c1c8055804f8c66c05a934e150995e2b8658dc
nicolas <linze201@gmail.com>
2024-03-10 12:31:13 +0800

    because "All <FileHandle> objects are <EventEmitter>s.", refactoring the code to use the EventEmitter API

--- file-system/open.js
+++ file-system/open.js
@@ -1,34 +1,36 @@
-// using the open method of the fs module to create a filehandle instance to read the contents of a file.
-
+// because "All <FileHandle> objects are <EventEmitter>s.", refactoring the code to use the EventEmitter API
 const fs = require('node:fs/promises');
 
 (async () => {
   const commandFileHandler = await fs.open('./command.txt', 'r');
 
+  commandFileHandler.on('change', async () => {
+    // get the size of the file
+    const size = (await commandFileHandler.stat()).size;
+    // allocate our buffer with the size of the file
+    const buff = Buffer.alloc(size);
+    // the location at which we want to start filling out buffer
+    const offset = 0;
+    // how many bytes we want to read
+    const length = buff.byteLength;
+    // the position that we want to start reading the file from
+    const position = 0;
+
+    // read from the first character to the end of the file
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
+
   const watcher = fs.watch('./command.txt');
 
   for await (const event of watcher) {
     if (event.eventType === 'change') {
-      console.log('The file was changed');
-
-      // get the size of the file
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-
-      const offset = 0;
-      const length = buff.byteLength;
-      const position = 0;
-
-      // const content = await commandFileHandler.read(Buffer.alloc(size)); // no data in the buffer
-
-      // read from the first character to the end of the file
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position
-      );
-      console.log(content);
+      commandFileHandler.emit('change');
     }
   }
 })();
